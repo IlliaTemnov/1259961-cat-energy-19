c3e1f6deee0dc050feae9d4eb17a186d088ebabd
IlliaTemnov <ilyatemnov@gmail.com>
2020-05-10 16:30:41 +0300

    Update 10.05 --2

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var sourcemap = require('gulp-sourcemaps');
-var less = require('gulp-less');
-var postcss = require('gulp-postcss');
-var imagemin = require('gulp-imagemin');
-var webp = require('gulp-webp');
-var posthtml = require('gulp-posthtml');
-var include = require('posthtml-include');
-var autoprefixer = require('autoprefixer');
-var server = require('browser-sync').create();
-var del = require('del');
-var svgstore = require('gulp-svgstore');
-var rename = require('gulp-rename');
-var csso = require('gulp-csso');
-var htmlmin = require('gulp-htmlmin');
-var csso = require('gulp-csso');
-var uglify = require('gulp-uglify');
-var pipeline = require('readable-stream').pipeline;
-var concat = require('gulp-concat');
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var less = require("gulp-less");
+var postcss = require("gulp-postcss");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var del = require("del");
+var svgstore = require("gulp-svgstore");
+var rename = require("gulp-rename");
+var csso = require("gulp-csso");
+var htmlmin = require("gulp-htmlmin");
+var csso = require("gulp-csso");
+var uglify = require("gulp-uglify");
+var pipeline = require("readable-stream").pipeline;
+var concat = require("gulp-concat");
 
 
-gulp.task('css', function () {
-  return gulp.src('source/less/style.less')
+gulp.task("css", function () {
+  return gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
@@ -31,98 +31,98 @@ gulp.task('css', function () {
       autoprefixer()
     ]))
     .pipe(csso())
-    .pipe(rename('style.min.css'))
-    .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest('build/css'))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task('images', function () {
-  return gulp.src('source/img/*.{png, jpg, svg}')
+gulp.task("images", function () {
+  return gulp.src("source/img/*.{png, jpg, svg}")
     .pipe(imagemin([
       imagemin.optipng({ optimizationLevel: 3 }),
       imagemin.mozjpeg({ progressive: true }),
       imagemin.svgo(),
     ]))
-    .pipe(gulp.dest('source/img'))
+    .pipe(gulp.dest("source/img"))
 });
 
-gulp.task('webp', function () {
-  return gulp.src('source/img/*.{png, jpg}')
+gulp.task("webp", function () {
+  return gulp.src("source/img/*.{png, jpg}")
     .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest('source/img'))
+    .pipe(gulp.dest("source/img"))
 });
 
-gulp.task('server', function () {
+gulp.task("server", function () {
   server.init({
-    server: 'build/',
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch('source/less/**/*.less', gulp.series('css'));
-  gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
-  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
+  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task('refresh', function (done) {
+gulp.task("refresh", function (done) {
   server.reload();
   done();
 });
 
 
-gulp.task('copy', function () {
+gulp.task("copy", function () {
   return gulp.src([
-    'source/fonts/*',
-    'source/img/**',
-    'source/*.ico'
+    "source/fonts/*",
+    "source/img/**",
+    "source/*.ico"
   ], {
-    base: 'source'
+    base: "source"
   })
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest("build"));
 });
 
-gulp.task('clean', function () {
-  return del('build');
+gulp.task("clean", function () {
+  return del("build");
 });
 
-gulp.task('sprite', function () {
-  return gulp.src('source/img/icon-*.svg')
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img'))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
 });
 
-gulp.task('html', function () {
-  return gulp.src('source/*.html')
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
     .pipe(posthtml([
       include()]))
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest("build"))
 });
 
-gulp.task('compress', function () {
-  return gulp.src('source/js/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('build/js'))
+gulp.task("compress", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest("build/js"))
     .pipe(uglify())
-    .pipe((rename('main.min.js')))
-    .pipe(gulp.dest('build/js'));
+    .pipe((rename("main.min.js")))
+    .pipe(gulp.dest("build/js"));
 });
 
-gulp.task('build', gulp.series(
-  'clean',
-  'copy',
-  'css',
-  'sprite',
-  'html',
-  'compress'
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html",
+  "compress"
 ));
 
-gulp.task('start', gulp.series(
-  'build',
-  'server'));
+gulp.task("start", gulp.series(
+  "build",
+  "server"));
--- source/js/map.js
+++ source/js/map.js
@@ -3,10 +3,10 @@ function initMap() {
   var office = { lat: 59.938564, lng: 30.323716 };
   // The map, centered at Office
   var map = new google.maps.Map(
-    document.getElementById('map'), { zoom: 17, center: office });
+    document.getElementById("map"), { zoom: 17, center: office });
   // The marker, positioned at Office
   var marker = new google.maps.Marker({
     position: office, map: map,
-    icon: './img/map-pin.png'
+    icon: "./img/map-pin.png"
   });
 }
